7aa4db77dd179666ab54e469acee4d02baf512a1
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-11-24 20:55:07 -0500

    updated footer component and styles

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -3,7 +3,44 @@ import "./footer.styles.css"
 const Footer = () => {
   return (
     <footer className="footer">
-      Footer
+      <div className="footer__inner">
+        <h3 className="footer__title">ThinkMath</h3>
+        <div className="footer__grid">
+          <a href="/" className="footer__link">
+            Footer links would go here
+          </a>
+          <a href="/" className="footer__link">
+            Dummy er linkser linksLink
+          </a>
+          <a href="/" className="footer__link">
+            Dummyer linkser links Link
+          </a>
+          <a href="/" className="footer__link">
+            Dummyer linkser linkser linkser linkser linkser links Link
+          </a>
+          <a href="/" className="footer__link">
+            Dummy Link
+          </a>
+          <a href="/" className="footer__link">
+            Dummy Link
+          </a>
+          <a href="/" className="footer__link">
+            Dummy Link
+          </a>
+          <a href="/" className="footer__link">
+            Dummy Link
+          </a>
+          <a href="/" className="footer__link">
+            Dummy Link
+          </a>
+          <a href="/" className="footer__link">
+            Dummy Link
+          </a>
+        </div>
+        <p className="footer__author">
+          Created by: Yatrik Patel, November 28, 2019
+        </p>
+      </div>
     </footer>
   )
 }
