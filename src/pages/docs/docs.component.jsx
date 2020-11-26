e8c73969fd688b2b771c8298995b1c3a98c1982f
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-11-25 23:16:40 -0500

    added pages folder

---
+++ src/pages/docs/docs.component.jsx
@@ -0,0 +1,11 @@
+import "./docs.styles.css"
+
+const Docs = () => {
+  return (
+    <div>
+      Docs
+    </div>
+  )
+}
+
+export default Docs
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import MainComponent from "../../components/main/main.component"
+import "./homepage.styles.css"
+
+const Homepage = () => {
+  return (
+    <div>
+      <MainComponent />
+    </div>
+  )
+}
+
+export default Homepage
