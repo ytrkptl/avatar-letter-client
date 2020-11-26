7613a1ee9aec7ac1ee515cbc5bbddf3b82452e65
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-11-26 18:03:59 -0500

    Added credit component

---
+++ src/components/credits/credits.component.jsx
@@ -0,0 +1,17 @@
+import "./credits.styles.css"
+
+const Credits = () => {
+  return (
+    <div id="credits">
+      <h2>Credits</h2>
+      <ol>
+        <li>
+          The images were downloaded from Freepik and later modified. Here's the direct link: 
+          <a href="http://www.freepik.com" target="_blank" rel="noreferrer" style={{display: "block"}}>Designed by Sapann-Design / Freepik</a>
+        </li>
+      </ol>
+    </div>
+  )
+}
+
+export default Credits
