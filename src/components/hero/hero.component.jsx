f39528a96d2b54eab620a79cca4271532be02627
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-11-27 08:30:52 -0500

    updated hero component styles and main component text

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -3,7 +3,7 @@ import "./hero.styles.css";
 const Hero = ({scrollTo}) => {
   return (
     <div className="hero">
-      <h1>Avatar Letter</h1>
+      <h1 className="hero-h1">Avatar Letter</h1>      
       <p>A 3D-ish style avatar!</p>
       <button className="hero-button" onClick={()=>scrollTo("how-to-get-started")}>Get Started</button>
     </div>
--- src/components/main/main.component.jsx
+++ src/components/main/main.component.jsx
@@ -33,7 +33,7 @@ const MainComponent = ({scrollTo}) => {
            I made this to use in another app I'm working on so I don't have to rely on other Avatar libraries or providers, or maybe just for fun.
         </p>
         <p className="article-para">
-            The idea is to return a beautiful 3D-ish looking letter's image to the user rather than the default ones we see in most applications while making sure to provide a variety of sizes, types (1 for now), and formats (2 for now) for free. They can also be non 3D-ish, but for now, it only contains one type, called type1, which looks 3D-ish.
+            The idea is to return a beautiful 3D-ish looking letter's image or rather unique ones to the user rather than the default ones we see in most applications while making sure to provide a variety of sizes, sets (1 for now), and formats (2 for now) for free. They can also be non 3D-ish, but for now, it only contains one set, called set1, which looks 3D-ish.
         </p>
         <div className="avatar-letter-img-container">
           <img src={`${process.env.REACT_APP_API_URL}/api/file/set1/tiny/a/webp`} alt="avatar letter tiny a" className="avatar-letter-img" />
@@ -43,14 +43,14 @@ const MainComponent = ({scrollTo}) => {
           <img src={`${process.env.REACT_APP_API_URL}/api/file/set1/large/a/webp`} alt="avatar letter large a" className="avatar-letter-img" /> 
           <img src={`${process.env.REACT_APP_API_URL}/api/file/set1/full/a/webp`} alt="avatar letter full a" className="avatar-letter-img" /> 
         </div>
-        <span className="img-description">3D-ish looking avatar-letter provided by avatar-letter</span>
+        <span className="img-description">Stylish avatar-letter provided by avatar-letter</span>
         <img alt="samples avatar-letter found elsewhere" src={FoundElsewhere} className="avatar-letter-img avatar-letter-img-2" height="400px" />
         <span className="img-description img-description-2">avatar-letters found elsewhere in component or other libraries</span>
         <p className="article-para">
           The images were edited in PowerPoint initially after downloading from Freepik first. The images were then resized, compressed, and saved in other formats using the Sharp library. It's crazy how sharp can save hundreds of images on your device in a split of a second.
         </p>
         <p className="article-para">
-          As far as creating, updating, or deleting any so-called "types" as I mentioned above, sizes, or formats (basically the static images found in public/letters folder) is concerned, I'd like to be the one maintaining that right now. I will consider ideas for improvement, or ideas for expanding this and providing users with more options
+          As far as creating, updating, or deleting any sets, sizes, or formats (basically the static images found in public/letters folder) is concerned, I'd like to be the one maintaining that right now. I will consider ideas for improvement, or ideas for expanding this and providing users with more options
         </p>
         <h2 id="how-to-get-started">How to use this/How to Get Started?</h2>
         <h4>To use this in your project, the steps are simple:</h4>
