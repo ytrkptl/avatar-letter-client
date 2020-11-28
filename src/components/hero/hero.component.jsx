c7f7afb12b7fe4bc4bd13738548190537c921b04
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-11-28 11:09:36 -0500

    updated text in hero

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -4,7 +4,7 @@ const Hero = ({scrollTo}) => {
   return (
     <div className="hero">
       <h1 className="hero-h1">Avatar Letter</h1>      
-      <p>A 3D-ish style avatar!</p>
+      <p className="hero-p">A 3D-ish, stylish, cool, or unique-looking avatar!</p>
       <button className="hero-button" onClick={()=>scrollTo("how-to-get-started")}>Get Started</button>
     </div>
   )
