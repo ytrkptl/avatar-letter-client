035065ea215dced620f67ccdb3e253261c4fd8d5
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-11-28 10:48:32 -0500

    added credits for set 2

--- src/components/credits/credits.component.jsx
+++ src/components/credits/credits.component.jsx
@@ -4,12 +4,13 @@ const Credits = () => {
   return (
     <div id="credits">
       <h2>Credits</h2>
-      <ol>
-        <li>
-          The images were downloaded from Freepik and later modified. Here's the direct link: 
-          <a href="http://www.freepik.com" target="_blank" rel="noreferrer" style={{display: "block"}}>Designed by Sapann-Design / Freepik</a>
-        </li>
-      </ol>
+      <p>The images were downloaded from Freepik and later modified. Here are the direct links:</p>
+      <div className="credits-list">
+        <span>Set 1:</span>
+        <a href="https://www.freepik.com/free-vector/colorful-alphabet_958008.htm" target="_blank" rel="noreferrer" style={{display: "block"}}>Designed by Sapann-Design / Freepik</a>
+        <span>Set 2:</span>
+        <a href="https://www.freepik.com/free-vector/hand-drawn-colorful-alphabet_2920859.htm" target="_blank" rel="noreferrer" style={{display: "block"}}>Designed by pikisuperstar / Freepik</a>
+      </div>
     </div>
   )
 }
