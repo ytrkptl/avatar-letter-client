15a6d8e927a165b87a8acf6af7ca85c1889872f9
Yatrik Patel <35569392+ytrkptl@users.noreply.github.com>
2025-12-06 11:20:08 -0500

    Changes let to const in scrollTo function for immutable variables parentRect, element, rect, and offset

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ function App() {
   // the following function accepts hashname or id
   // and scrolls to that id's location on the page
   const scrollTo = (hashName: string) => {
-    let parentRect = document.body.getBoundingClientRect();
-    let element = document.getElementById(hashName);
+    const parentRect = document.body.getBoundingClientRect();
+    const element = document.getElementById(hashName);
     if (element) {
-      let rect = element.getBoundingClientRect();
-      let offset = rect.top - parentRect.top - 20;
+      const rect = element.getBoundingClientRect();
+      const offset = rect.top - parentRect.top - 20;
       window.scrollTo(0, offset);
     }
   };
